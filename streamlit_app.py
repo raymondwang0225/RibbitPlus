ec2f91a5835341683a4c32292700174838055f9a
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-08 18:52:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,9 @@
 import streamlit as st
-
-# 定义触发的函数
-def on_link_click():
-    st.write('链接被点击了')
-    # 在此处添加您希望触发的 Streamlit 逻辑
-
-# 在 Streamlit 上显示 Markdown 链接
-link_text = 'Home'
-link_url = 'javascript: void(0);'
-markdown_link = f'<a href="{link_url}" onclick="event.preventDefault(); triggerLinkClick();">{link_text}</a>'
-st.markdown(markdown_link, unsafe_allow_html=True)
-
-# 调用触发的函数
-if 'on_link_click' in st.session_state:
-    on_link_click()
-    del st.session_state.on_link_click
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
