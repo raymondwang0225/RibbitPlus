6ffe7683607641b30e6acfcd8c9af19fc161e757
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-09 07:17:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,17 +18,17 @@ st.markdown(css, unsafe_allow_html=True)
 # 創建兩個 column
 col_a, col_b = st.columns(2)
 
-st.markdown('<div class="my-column">', unsafe_allow_html=True)
+
 # 在 col A 中套用 CSS 樣式
 with col_a:
-    
-    st.multiselect("選擇項目", ["選項1", "選項2", "選項3"])
-    st.multiselect("選擇項目", ["選項1", "選項2", "選項3"])
-    st.multiselect("選擇項目", ["選項1", "選項2", "選項3"])
-    st.multiselect("選擇項目", ["選項1", "選項2", "選項3"])
-    
+    st.markdown('<div class="my-column">', unsafe_allow_html=True)
+    st.write("這是 col A")
+    st.write("這是 col A")
+    st.write("這是 col A")
+    st.write("這是 col A")
+    st.markdown('</div>', unsafe_allow_html=True)
 
 # 在 col B 中顯示文字
-st.markdown('</div>', unsafe_allow_html=True)
+
 with col_b:
     st.write("這是 col B")
