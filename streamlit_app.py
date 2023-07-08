3b56d5f660d1309da2935174d673dde45af9447b
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-09 06:18:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ aa="""
 <style>
 div[data-testid="metric-container"] {
    border: 1px solid rgba(255, 255, 255, 0.25);
-   padding: 5% 5% 5% 5%;
+   padding: 20px 20px 20px 20px;
    border-radius: 12px;
    color: rgb(255, 255, 255);
 }
