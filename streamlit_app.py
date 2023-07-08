545fca2a21d3d181a2f599022c3ce969a69f3e26
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-08 21:29:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,13 +23,7 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
 
 # 1=sidebar menu, 2=horizontal menu, 3=horizontal menu w/ custom menu
 EXAMPLE_NO = 1
@@ -88,6 +82,13 @@ def streamlit_menu(example=1):
 selected = streamlit_menu(example=EXAMPLE_NO)
 
 if selected == "Home":
+	with open('style.css') as f:
+	    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+	
+	col1, col2, col3 = st.columns(3)
+	col1.metric("Temperature", "70 °F", "1.2 °F")
+	col2.metric("Wind", "9 mph", "-8%")
+	col3.metric("Humidity", "86%", "4%")
     st.title(f"You have selected {selected}")
 if selected == "Projects":
     st.title(f"You have selected {selected}")
