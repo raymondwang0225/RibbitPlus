399ca59b414e433a6480e8121018e59af21ea588
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-08 21:24:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ st.markdown(
 )
 
 with open('style.css') as f:
-  st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 # 1=sidebar menu, 2=horizontal menu, 3=horizontal menu w/ custom menu
 EXAMPLE_NO = 1
