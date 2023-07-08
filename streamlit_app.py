8bfeb3a6c09d141087fa0f8637f0795fe30bcb74
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-09 01:04:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ eyes = ["Dank Shades", "Happy", "Visor", "Monocle", "none", "Frown", "Powerful",
 
 
 #1=sidebar menu, 2=horizontal menu, 3=horizontal menu w/ custom menu
-EXAMPLE_NO = 2
+EXAMPLE_NO = 1
 
 
 def streamlit_menu(example=1):
