fa28e34990d6fd4844df987f47085b3b11b9d63d
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-08 18:46:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,17 @@
 import streamlit as st
 
-# 定義觸發的函數
+# 定义触发的函数
 def on_link_click():
-    st.write('連結被點擊了')
-    # 在此處加入您希望觸發的 Streamlit 邏輯
+    st.write('链接被点击了')
+    # 在此处添加您希望触发的 Streamlit 逻辑
 
-# 顯示 Streamlit 按鈕，設定樣式為看起來像連結
-link_style = '''
-    background: none;
-    border: none;
-    padding: 0;
-    color: blue;
-    text-decoration: underline;
-    cursor: pointer;
-'''
+# 在 Streamlit 上显示 Markdown 链接
+link_text = 'Home'
+link_url = 'javascript: void(0);'
+markdown_link = f'<a href="{link_url}" onclick="event.preventDefault(); triggerLinkClick();">{link_text}</a>'
+st.markdown(markdown_link, unsafe_allow_html=True)
 
-if st.button('Home', help='連結', unsafe_allow_html=True, key='link_button', on_click=on_link_click):
-    pass  # 無需執行任何操作，僅為確保按鈕顯示
-
-# 透過 CSS 設定按鈕樣式
-st.markdown(f'<style>{link_style}</style>', unsafe_allow_html=True)
+# 调用触发的函数
+if 'on_link_click' in st.session_state:
+    on_link_click()
+    del st.session_state.on_link_click
