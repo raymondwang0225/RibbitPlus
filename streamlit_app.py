85719a6ccf2c24d1c78f45f0693551365c829764
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-08 21:14:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,6 +80,9 @@ def streamlit_menu(example=1):
 selected = streamlit_menu(example=EXAMPLE_NO)
 
 if selected == "Home":
+    with open('style.css') as f:
+     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
     st.title(f"You have selected {selected}")
 if selected == "Projects":
     st.title(f"You have selected {selected}")
