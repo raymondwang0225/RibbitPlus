69f4822567addd35e050382b568dd06856b66002
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-08 22:21:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,8 +38,8 @@ def streamlit_menu(example=1):
         with st.sidebar:
             selected = option_menu(
                 menu_title="RIBBIT PLUS",  # required
-                options=["Home", "Projects", "Contact"],  # required
-                icons=["house", "book", "envelope"],  # optional
+                options=["Home", "Projects", "Contact","Filter","Weekly Report"],  # required
+                icons=["house", "book", "envelope","bi bi-chat-square-dots-fill","bi bi-archive"],  # optional
                 menu_icon="bi bi-arrow-up-left-square",  # optional
                 default_index=0,  # optional
             )
@@ -50,7 +50,7 @@ def streamlit_menu(example=1):
         st.title("Ribbit Plus")
         selected = option_menu(
             menu_title=None,  # required
-            options=["Home", "Projects", "Contact",],  # required
+            options=["Home", "Projects", "Contact"],  # required
             icons=["house", "book", "envelope"],  # optional
             menu_icon="cast",  # optional
             default_index=0,  # optional
