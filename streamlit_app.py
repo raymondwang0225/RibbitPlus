92735074168906bec04f2df6a136d832f75f682f
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-09 09:11:57 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -221,7 +221,7 @@ if selected == "Filter":
                     #st.markdown(caption, unsafe_allow_html=True)
                     # 顯示動態內容的標題
                     if st.button(link_name):
-                        
+                        st.empty()
                         st.write("執行自定義函數")
 
 
