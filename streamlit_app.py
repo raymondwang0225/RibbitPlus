57ce9056827e6a1d7facdb6830d0164d34f15050
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-09 08:21:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -237,15 +237,6 @@ if modal.is_open():
     with modal.container():
         st.write("Text goes here")
 
-        html_string = '''
-        <h1>HTML string in RED</h1>
-
-        <script language="javascript">
-          document.querySelector("h1").style.color = "red";
-        </script>
-        '''
-        components.html(html_string)
-
         st.write("Some fancy text")
         value = st.checkbox("Check me")
         st.write(f"Checkbox checked: {value}")
