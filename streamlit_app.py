5fc532ddd024fb17b0a26873a90f4350a3f4497a
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-10 17:00:12 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -180,9 +180,9 @@ if selected == "Filter":
 
     st.title("Filter")
     #st.markdown("<hr/>", unsafe_allow_html = True)
+    with st.expander("aaa"):
+        col1, col2 ,col3, col4 , col5 ,col6 ,col7 = st.columns([1,1,1,1,1,1,1])
     
-    col1, col2 ,col3, col4 , col5 ,col6 ,col7 = st.columns([1,1,1,1,1,1,1])
-    with st.expander("Status"):
         with col1:
             desired_satyears = st.multiselect("Sat Year", satyears)
         
