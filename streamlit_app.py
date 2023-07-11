916a80b4dea7cd757028639a38aad2b8a3692c5f
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-11 14:07:03 +0800

    Update streamlit_app.py
    callback test2

--- streamlit_app.py
+++ streamlit_app.py
@@ -255,8 +255,7 @@ if selected == "Filter":
         column_value = st.selectbox("Column display quantity", (1, 2, 3,4,5,6,7,8,9,10,11),label_visibility="hidden")
     
     
-
- '''           
+'''           
     # "Apply Filter" 按钮
     apply_filter = st.button("Apply Filter")   
 
